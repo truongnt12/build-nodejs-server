9b02d33ced9ddf8512f624f3e7e40491c4ee0082
TruongNT <truongnt7@fpt.com.vn>
2021-09-14 01:19:54 +0700

    update server.js

--- server.mjs
+++ server.mjs
@@ -4,7 +4,7 @@ import registerMiddlewares  from "./middlewares/index.mjs";
 
 async function main(){
   registerMiddlewares(server);
-  server.all("/", (req, res) => res.redirect("/v1"));
+  //server.all("/", (req, res) => res.redirect("/v1"));
   server.use("/v1", rootRouter);
 
   server.listen();
